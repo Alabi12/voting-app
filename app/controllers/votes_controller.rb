9026dc550065178428ed953620fc65e716a64f2f
Robert O. Alabi <wolo22001@gmail.com>
2024-10-12 13:47:59 +0000

    add percentage analysis to the app

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,20 +4,30 @@ class VotesController < ApplicationController
   def vote
     candidate = Candidate.find(params[:candidate_id])
 
+    # Check if the user has already voted for a candidate in this position
     if current_user.voted_for?(candidate.position)
-      redirect_to position_path(candidate.position), alert: "You have already voted in this position."
+      redirect_to position_path(candidate.position), alert: "You have already voted for your favorite candidate."
     else
       # Create the vote
       Vote.create(candidate: candidate, user: current_user, position: candidate.position)
 
-      # Update the vote count
-      candidate.increment!(:vote_count)
+      # Set success flash message
+      flash[:notice] = "Your vote for #{candidate.name} has been successfully cast!"
 
-      redirect_to position_path(candidate.position), notice: "Your vote for #{candidate.name} has been cast!"
+      redirect_to position_path(candidate.position)
     end
   end
 
   def summary
     @positions = Position.includes(candidates: :votes)  # Preload candidates and their votes
+
+    # Calculate total votes for each position
+    @positions.each do |position|
+      total_votes = position.candidates.joins(:votes).count
+      
+      position.candidates.each do |candidate|
+        candidate.vote_percentage = total_votes.zero? ? 0 : (candidate.votes.count.to_f / total_votes * 100).round(2)
+      end
+    end
   end
 end
