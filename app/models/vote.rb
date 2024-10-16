b43acbc817fd5c2f045bd6702406e99b35cb400c
Robert O. Alabi <wolo22001@gmail.com>
2024-10-16 17:09:45 +0000

    fixing render issues

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ApplicationRecord
   belongs_to :position
   belongs_to :candidate, counter_cache: true
 
-  validates :user_id, uniqueness: { scope: [:candidate_id, :position_id] } # Ensure one vote per user per candidate per position
+  # validates :user_id, uniqueness: { scope: [:candidate_id, :position_id] } # Ensure one vote per user per candidate per position
 end
